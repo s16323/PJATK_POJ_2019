0ddc316e0b0b2ecdb9b7e0827ff4c4b7eae4c3cd
Artur Dembicki <artur.dembicki@gmail.com>
2019-05-10 20:35:40 +0200

    add EXERCISES04_01.java

---
+++ src/EXERCISES04_01.java
@@ -0,0 +1,32 @@
+/*
+Cw 4
+Zad 1
+http://szuflandia.pjwstk.edu.pl/~pczapiewski/POJ_2018-2019/POJ_cwiczenia_04_2019.04.27.pdf
+*/
+
+class Person {
+
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String toString(){
+        return "Account[name= " + name + ", address=" + address + "]";
+    }
+}
