5299a4e569135fa19ab1e2945636c377da97c7d8
Artur Dembicki <artur.dembicki@gmail.com>
2019-05-18 11:30:42 +0200

    add Owiczenia05 directory and MyPoint.java task

--- Cwiczenia05/EXERCISES05_01/MyPoint.java
+++ Cwiczenia05/EXERCISES05_01/MyPoint.java
@@ -4,7 +4,8 @@ Cw 5, Zad 1
 http://szuflandia.pjwstk.edu.pl/~pczapiewski/POJ_2018-2019/POJ_cwiczenia_05_2019.05.11.pdf
 
  */
-
+import java.lang.Math;
+import java.lang.reflect.Array;
 
 public class MyPoint {
 
@@ -53,10 +54,42 @@ public class MyPoint {
         return "(" + x + "," + y + ")";
     }
 
-    public double distance(int x, int y){
+//    public double distance(int x, int y){
+//
+//
+//    }
 
+    public double distance(MyPoint another){
 
+        int x0 = another.getX();
+        int y0 = another.getY();
+
+//    double dist = java.lang.Math.sqrt(Math.pow((this.x - x0), 2)+ Math.pow((this.y - y0), 2));
+        double dist = Math.sqrt((this.x - x0)^2 + (this.y - y0)^2);
+        return dist;
     }
 
+    public double distance(){
 
+        double dist = Math.sqrt((this.x)^2 + (this.y)^2);
+        return dist;
+    }
+
+}
+
+class Main {
+
+    public static void main(String[] args) {
+
+        MyPoint myPoint = new MyPoint(1,1);
+
+        //Array<MyPoint> list = new Array();
+        List<MyPoint> list = new
+
+        for (i = 1, i <= list.length, i++){
+
+//            list.add(MyPoint(i,i));
+
+        }
+    }
 }
