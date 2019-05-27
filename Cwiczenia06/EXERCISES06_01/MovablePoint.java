eefb0ff2ea2af7c9e63befea34540b90fc35ec5a
Artur Dembicki <artur.dembicki@gmail.com>
2019-05-27 14:03:07 +0200

    add EXERCISES06_01

--- Cwiczenia06/EXERCISES06_01/MovablePoint.java
+++ Cwiczenia06/EXERCISES06_01/MovablePoint.java
@@ -1,2 +1,34 @@
-public class MovablePoint {
+public class MovablePoint implements Movable {
+
+    //Instance variables:
+    int x, y, xSpeed, ySpeed;   //package access
+
+    //Constructor:
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    //Abstract methods declared in Movable interface:
+    @Override
+    public void moveUp() {
+        y -= ySpeed;    //y-axis points down
+    }
+
+    @Override
+    public void moveDown() {
+        y += ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        x -= xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        x += xSpeed;    //x-axis points right
+    }
 }
--- Cwiczenia06/EXERCISES06_01/MovableRectangle.java
+++ Cwiczenia06/EXERCISES06_01/MovableRectangle.java
@@ -10,31 +10,31 @@ public class MovableRectangle implements Movable {
 
     public MovableRectangle(int x1, int y1, int x2, int y2, int xSpeed, int ySpeed) {
 
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y1 = y2;
-        this.xSpeed = xSpeed;
-        this.ySpeed = ySpeed;
+        this.topLeft = new MovablePoint(x1, y1, xSpeed, ySpeed);
+        this.bottomRight = new MovablePoint(x2, y2, xSpeed, ySpeed);
     }
 
     @Override
     public void moveUp() {
-
+        topLeft.moveUp();
+        bottomRight.moveUp();
     }
 
     @Override
     public void moveDown() {
-
+        topLeft.moveDown();
+        bottomRight.moveDown();
     }
 
     @Override
     public void moveLeft() {
-
+        topLeft.moveLeft();
+        bottomRight.moveLeft();
     }
 
     @Override
     public void moveRight() {
-
+        topLeft.moveRight();
+        bottomRight.moveRight();
     }
 }
