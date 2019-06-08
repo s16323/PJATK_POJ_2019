5e84803f74ff9b68a2e482d77c3fdadf4f755726
Walec <s16323@pjwstk.edu.pl>
2019-06-08 11:17:01 +0200

    small changes to Insurance EXERCISES06_03

--- Cwiczenia06/Insurance/Insurance.java
+++ Cwiczenia06/Insurance/Insurance.java
@@ -1,8 +1,13 @@
 /*
-Autorzy: Aleksander Mojzych, Artur Dembicki  25.05.2019
+Zad 3 Cwiczenia 6
+Autorzy:    Aleksander Mojzych
+            Artur Dembicki 16323
+            25.05.2019
+
+http://szuflandia.pjwstk.edu.pl/~pczapiewski/POJ_2018-2019/POJ_cwiczenia_06_2019.05.25.pdf
  */
 
-package Insurance;
+//package Insurance;
 
 abstract class Insurance {
 
